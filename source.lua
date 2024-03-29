5890486bc0a171a7d4915d5fda4ee6ed821524e3
dariusplayz1 <97094335+dariusplayz1@users.noreply.github.com>
2024-03-29 12:17:15 -0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -1179,7 +1179,7 @@ do
                 --
                 healthvalue.Text = "<- " .. math.round(Smoothened * 100)
                 healthvalue.Color = Color
-                healthvalue.Position = utility:Position(0, 1, 1, -Size - 1, healthbaroutline)
+              
 		healthbar.Color = Color
                 healthbar.Size = utility:Size(1, -2, 0, Size, healthbaroutline)
                 healthbar.Position = utility:Position(0, 1, 1, -Size - 1, healthbaroutline)
