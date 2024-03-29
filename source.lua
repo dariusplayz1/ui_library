6bebe7df3ff1ab45e384c1f08358caca4074c4fc
dariusplayz1 <97094335+dariusplayz1@users.noreply.github.com>
2024-03-29 16:06:39 -0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -2634,11 +2634,7 @@ do
                     x.Visible = true
                 end
             end
-	    if window.currentPage.page_button_title.Text == "Visuals" and window and window.VisualPreview then
-		window.VisualPreview:SetPreviewState(true)
-	    else
-		window.VisualPreview:SetPreviewState(false)
-	    end
+
             --
             window.callback(name, window.currentPage)
             window:Move(window.main_frame.Position)
@@ -2647,6 +2643,11 @@ do
         library.began[#library.began + 1] = function(Input)
             if Input.UserInputType == Enum.UserInputType.MouseButton1 and window.isVisible and utility:MouseOverDrawing({page_button.Position.X,page_button.Position.Y,page_button.Position.X + page_button.Size.X,page_button.Position.Y + page_button.Size.Y}) and window.currentPage ~= page then
                 page:Show()
+	    if page.Name == "Visuals" and window and window.VisualPreview then
+		window.VisualPreview:SetPreviewState(true)
+	    else
+		window.VisualPreview:SetPreviewState(false)
+	    end
             end
         end
         --
